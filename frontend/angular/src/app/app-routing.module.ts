ae8f4c5663c518552c050758a3ba73488968f347
Jhon Murcia <jjmurcia.r19@gmail.com>
2021-12-09 23:06:57 -0500

    sing up

--- frontend/angular/src/app/app-routing.module.ts
+++ frontend/angular/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { VoidComponent } from './components/void/void.component';
+import { SingUpComponent } from './components/sing-up/sing-up.component'
 
 
 const routes: Routes = [
   {path:"", component: LoginComponent},
+  {path:"sing-up", component: SingUpComponent},
   {path:"home", component: HomeComponent},
   {path:"dashboard", component: DashboardComponent},
   {path:"**",component:VoidComponent}
