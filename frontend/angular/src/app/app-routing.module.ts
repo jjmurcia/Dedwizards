8ffe4637ff3f080cd1dec90277fa71a1857bcf94
Jhon Murcia <jjmurcia.r19@gmail.com>
2021-12-09 21:05:01 -0500

    login

--- frontend/angular/src/app/app-routing.module.ts
+++ frontend/angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { VoidComponent } from './components/void/void.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:"", component: LoginComponent},
+  {path:"home", component: HomeComponent},
+  {path:"dashboard", component: DashboardComponent},
+  {path:"**",component:VoidComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
